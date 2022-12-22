8434d0dfaf6df04732a6036c5ef84c5b3c239372
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-22 10:28:39 +0200

    Added components and routing in angular

---
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-detail-page',
+  templateUrl: './listings-detail-page.component.html',
+  styleUrls: ['./listings-detail-page.component.css']
+})
+export class ListingsDetailPageComponent {
+
+}
