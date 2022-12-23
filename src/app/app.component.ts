2096f24bcd1298c4ccef1f91b271649003a65e7b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-23 12:03:01 +0000

    'Logging and logging out with firebase'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'buy-and-sell';
+  constructor(public auth: AngularFireAuth) {}
+
+  signInClicked(): void {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  signOutClicked(): void {
+    this.auth.signOut();
+  }
 }
